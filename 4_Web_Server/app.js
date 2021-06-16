61245a4ad4283dec85a641166b91e3a5d2c9e011
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-16 16:48:20 -0400

    fix: Serving HTML using streams

--- 4_Web_Server/app.js
+++ 4_Web_Server/app.js
@@ -4,11 +4,6 @@ let fs = require('fs');
 http.createServer((req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    let html = fs.readFileSync(__dirname + '/index.htm', 'utf-8');
-    var message = 'Hello world...';
-
-    html = html.replace('{Message}', message);
-    
-    res.end(html);
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
 }).listen(4000, '127.0.0.1');
