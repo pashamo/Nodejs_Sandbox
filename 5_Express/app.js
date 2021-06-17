62b16ff61c109491a9f2a70922ada1edb245e973
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-17 13:21:49 -0400

    feat: Middleware to serve static files

--- 5_Express/app.js
+++ 5_Express/app.js
@@ -3,8 +3,14 @@ let app = express();
 
 let port = process.env.PORT || 4000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+app.use ('/', (req, res, next) => {
+    console.log('Request URL: ' + req.url);
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
 });
 
 app.get('/person/:id', (req, res) => {
