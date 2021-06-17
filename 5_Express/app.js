1069b5c2bedf2ece43331bbe283bd9bc8301d4c9
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-17 13:58:52 -0400

    feat: Querystrings and POST method

--- 5_Express/app.js
+++ 5_Express/app.js
@@ -1,7 +1,9 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
-
 let port = process.env.PORT || 4000;
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.use ('/', (req, res, next) => {
@@ -18,8 +20,18 @@ app.get('/', (req, res) => {
 
 app.get('/person/:id', (req, res) => {
     // res.send('<html><head></head><body><h1>Hello '+ req.params.id +'!</h1></body></html>');
-    res.render('person', { ID: req.params.id }); //using view engine
+    res.render('person', { ID: req.params.id, Qstr: req.query.qstr }); //using view engine
 });
+app.post('/person', urlencodedParser, (req, res) => {
+    res.send('Thank you!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+app.post('/personjson', jsonParser, (req, res) => {
+    res.send("Thank you for JSON data!");
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+})
 
 app.get('/api', (req, res) => {
     res.json({ firstname: 'Mohammed', lastname: 'Pasha'});
