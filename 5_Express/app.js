4fea16bbe3e77d3bfc0661359bbc72e483367b17
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-17 12:47:18 -0400

    feat: Using Pattern Matching routes

--- 5_Express/app.js
+++ 5_Express/app.js
@@ -5,10 +5,14 @@ let port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
-})
+});
+
+app.get('/person/:id', (req, res) => {
+    res.send('<html><head></head><body><h1>Hello '+ req.params.id +'!</h1></body></html>');
+});
 
 app.get('/api', (req, res) => {
     res.json({ firstname: 'Mohammed', lastname: 'Pasha'});
-})
+});
 
 app.listen(port);
