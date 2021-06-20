a91ed46520d621dab2dfb8b3fac73e657a0c12cf
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-19 23:47:51 -0400

    feat: Skeleton project setup

---
+++ 6_MEAN_Legacy/app.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+
+let port = process.env.PORT || 4000;
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+    res.render('index');
+
+});
+
+app.listen(port);
