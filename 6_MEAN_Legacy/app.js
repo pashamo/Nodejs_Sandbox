a8aa8691e7d2ab6b274287add44484dda4e9f21b
Mohammed Pasha <mk.pasha@hotmail.com>
2021-06-20 00:21:18 -0400

    fix: moved people JSON to server

--- 6_MEAN_Legacy/app.js
+++ 6_MEAN_Legacy/app.js
@@ -3,12 +3,24 @@ let app = express();
 
 let port = process.env.PORT || 4000;
 
+let people = [
+    {
+        name: 'John Doe'
+    },
+    {
+        name: 'Jane Doe'
+    },
+    {
+        name: 'Jene Doe'
+    }
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 
-    res.render('index');
+    res.render('index', { serverPeople: people });
 
 });
 
